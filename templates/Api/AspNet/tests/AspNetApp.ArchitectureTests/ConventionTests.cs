1735e7fcdbad555d56fdb2ab1f52a430452db93e
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-03 05:12:42 +0800

    Add architecture tests

---
+++ templates/Api/AspNet/tests/AspNetApp.ArchitectureTests/ConventionTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+using NetArchTest.Rules;
+using RichillCapital.SharedKernel;
+
+namespace AspNetApp.ArchitectureTests;
+
+public sealed class ConventionTests : ArchitectureTest
+{
+    [Fact]
+    public void DomainEvents_Should_HaveDomainEventSuffix()
+    {
+        var result = Types.InAssembly(DomainAssembly)
+            .That()
+            .Inherit(typeof(IDomainEvent))
+            .Should()
+            .HaveNameEndingWith("DomainEvent")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
