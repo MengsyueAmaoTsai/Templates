6166ff59e6d21aa0008f91a509901468e8b0df78
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-03 15:16:00 +0800

    remove nuxt

--- templates/Web/nuxt/src/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <NuxtRouteAnnouncer />
-  <NuxtLayout name="default">
-    <NuxtPage />
-  </NuxtLayout>
-</template>
-
-<script lang="ts" setup></script>
-<style lang="scss" scoped></style>
--- templates/Web/nuxt/src/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>Welcome to nuxt-app web</div>
-</template>
-
-<script lang="ts" setup></script>
-
-<style scoped lang="scss"></style>
