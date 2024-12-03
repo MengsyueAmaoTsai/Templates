1c8ba680ae03943c0e7c661ff2b872a7685e35a0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-03 19:48:08 +0800

    Default CTraderRobot template

---
+++ templates/SignalSource/CTrader/CTraderRobot/CTraderRobot.cs
@@ -0,0 +1,8 @@
+﻿using cAlgo.API;
+
+namespace RichillCapital.TradingBots.CTrader;
+
+[Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.FullAccess)]
+public sealed partial class CTraderRobot : Robot
+{
+}
