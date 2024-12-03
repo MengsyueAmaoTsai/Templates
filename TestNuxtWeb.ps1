6f5709910dde66a4b133980beb167a63924265fc
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-04 06:17:20 +0800

    Update template.json

---
+++ TestNuxtWeb.ps1
@@ -0,0 +1,42 @@
+$testFailed = $false
+
+$template = "./src/web/Nuxt"
+$projectDirectory = "./MyNuxtApp"
+
+dotnet new install $template --force
+
+dotnet new web-nuxt -n my-nuxt-app -o $projectDirectory
+Push-Location $projectDirectory
+
+if (Test-Path "./.nuxt") {
+    Write-Host "Test failed: .nuxt directory should not exist."
+    $testFailed = $true
+}
+
+if (Test-Path "./.output") {
+    Write-Host "Test failed: .output directory should not exist."
+    $testFailed = $true
+}
+
+if (Test-Path "./node_modules") {
+    Write-Host "Test failed: node_modules directory should not exist."
+    $testFailed = $true
+}
+
+if (Test-Path "./.template.config") {
+    Write-Host "Test failed: .template.config directory should not exist."
+    $testFailed = $true
+}
+
+if (-not $testFailed) {
+    Write-Host "All tests passed."
+}
+
+pnpm i 
+pnpm run build
+
+# Clean up
+Pop-Location
+if (Test-Path $projectDirectory) {
+    Remove-Item -Recurse -Force $projectDirectory
+}
