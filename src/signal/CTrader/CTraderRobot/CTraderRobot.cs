0b29ef97cac260e8b859f5bfad58786dbf7784c3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-04 23:59:45 +0800

    Create default robot template for ctrader

---
+++ src/signal/CTrader/CTraderRobot/CTraderRobot.cs
@@ -0,0 +1,10 @@
+﻿using cAlgo.API;
+
+namespace CTraderRobot;
+
+[Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.FullAccess)]
+public sealed partial class CTraderRobot : Robot
+{
+    protected override void OnStart() =>
+        Print("Hello world!");
+}
