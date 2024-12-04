c488e84fd13f37641dfb1c94490a0d09aadbd4a7
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-04 23:51:21 +0800

    Update pipeline

---
+++ tests/signal/TradingViewTests.ps1
@@ -0,0 +1,24 @@
+$testFailed = $false
+
+$template = "./src/signal/TradingView"
+$projectDirectory = "./MyTradingViewSignalSource"
+
+dotnet new install $template --force
+
+dotnet new web-nuxt -n MyTradingViewSignalSource -o $projectDirectory
+Push-Location $projectDirectory
+
+if (Test-Path "./.template.config") {
+    Write-Host "Test failed: .template.config directory should not exist."
+    $testFailed = $true
+}
+
+if (-not $testFailed) {
+    Write-Host "All tests passed."
+}
+
+# Clean up
+Pop-Location
+if (Test-Path $projectDirectory) {
+    Remove-Item -Recurse -Force $projectDirectory
+}
