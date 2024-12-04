87fc7b633acc08141f7ea6705a66ce75c031b759
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-04 10:07:25 +0800

    Empty gata grid display

---
+++ src/web/Nuxt/src/pages/users/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div style="display: flex">
+    <button>New user</button>
+    <button>Delete</button>
+    <button>Refresh</button>
+  </div>
+
+  <div>
+    <input type="search" placeholder="Search" />
+  </div>
+
+  <div>
+    <p>{{ users.length }} user{{ users.length > 1 ? "s" : "" }} found</p>
+  </div>
+
+  <table>
+    <thead>
+      <tr>
+        <th>
+          <input type="checkbox" />
+        </th>
+        <th>Id</th>
+        <th>Email</th>
+        <th>Name</th>
+        <th>Created time</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-if="users.length === 0">
+        <td>No results.</td>
+      </tr>
+
+      <tr v-for="user in users" :key="user.id">
+        <td>
+          <input type="checkbox" />
+        </td>
+        <td>{{ user.id }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.created_at }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts" setup>
+const users = ref([]);
+</script>
