7f714abff435be4b5d9b4c485dd9acbd22600a19
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-18 07:15:53 +0800

    Update optimization fitness value

--- src/strategy/CTrader/CTraderRobot/CTraderRobot.Optimization.cs
+++ src/strategy/CTrader/CTraderRobot/CTraderRobot.Optimization.cs
@@ -7,6 +7,6 @@ public sealed partial class CTraderRobot
     protected override double GetFitness(GetFitnessArgs args)
     {
         var netProfitToDrawdownRatio = args.NetProfit / Math.Abs(args.MaxEquityDrawdown);
-        return netProfitToDrawdownRatio / 3 * args.NetProfit;
+        return netProfitToDrawdownRatio / 3 * args.ProfitFactor;
     }
 }
