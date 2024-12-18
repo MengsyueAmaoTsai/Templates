43a31865215d41f519335f589e69d9f9e22a0b1f
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-18 12:56:34 +0800

    add solution file and remove min value

--- src/strategy/CTrader/CTraderRobot/CTraderRobot.Inputs.cs
+++ src/strategy/CTrader/CTraderRobot/CTraderRobot.Inputs.cs
@@ -7,18 +7,18 @@ public sealed partial class CTraderRobot
     [Parameter("Label", DefaultValue = "CTraderRobot")]
     public string Label { get; init; } = string.Empty;
 
-    [Parameter("Order size", DefaultValue = 30_000, MinValue = 100)]
+    [Parameter("Order size", DefaultValue = 30_000)]
     public int OrderSize { get; init; }
 
-    [Parameter("Channel length", DefaultValue = 55, MinValue = 5)]
+    [Parameter("Channel length", DefaultValue = 55)]
     public int ChannelLength { get; init; }
 
-    [Parameter("ATR length", DefaultValue = 14, MinValue = 5)]
+    [Parameter("ATR length", DefaultValue = 14)]
     public int AtrLength { get; init; }
 
-    [Parameter("Stop loss ATR multiplier", DefaultValue = 2.0, MinValue = 0.1)]
+    [Parameter("Stop loss ATR multiplier", DefaultValue = 2.0)]
     public double StopLossAtrMultiplier { get; init; }
 
-    [Parameter("Profit target ATR multiplier", DefaultValue = 2.0, MinValue = 0.1)]
+    [Parameter("Profit target ATR multiplier", DefaultValue = 2.0)]
     public double ProfitTargetAtrMultiplier { get; init; }
 }
