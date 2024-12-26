73da54ddbe1c1573bcac5d0280469ffb8bd40c10
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-26 11:12:08 +0800

    update route

--- src/web/RazorPages/src/RazorPagesApp.Web/Pages/Errors/Index.cshtml.cs
+++ src/web/RazorPages/src/RazorPagesApp.Web/Pages/Errors/Index.cshtml.cs
@@ -2,11 +2,11 @@ using System.Diagnostics;
 
 using Microsoft.AspNetCore.Mvc;
 
-namespace RazorPagesApp.Web.Pages;
+namespace RazorPagesApp.Web.Pages.Errors;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
-public sealed class ErrorViewModel : ViewModel
+public sealed class ErrorsViewModel : ViewModel
 {
     public string? RequestId { get; set; }
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
